a582b5e8661b1f7bc3bc8078319d72f724278b3b
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-25 12:14:46 +0330

    refactor : rename file

---
+++ db/currency/currency.utils.ts
@@ -0,0 +1,79 @@
+import { CurrencyDtoType, CurrencyModel, CurrencyType } from "../../models/currencyModel";
+import { connectToDB } from "../utils";
+
+export const CreateCurrency = async (
+    {
+        symbol,
+        name,
+        price,
+        currencyType,
+        cap,
+        percentChangeWeek,
+        percentChangeDay,
+        history
+    }:CurrencyDtoType
+) => {
+    try {
+        await connectToDB();
+        const currency = await CurrencyModel.create({
+            symbol,
+            name:{
+                en:name?.en,
+                fa:name?.fa
+            },
+            currencyType,
+            price,
+            cap,
+            percentChangeWeek,
+            percentChangeDay
+        });
+    } catch (err:any) {
+        throw new Error(err.messsage);
+    }
+};
+
+export const UpdateCurrency = async (
+    {
+        symbol,
+        name,
+        price,
+        currencyType,
+        cap,
+        percentChangeWeek,
+        percentChangeDay,
+        history
+    }:CurrencyDtoType
+) => {
+    try {
+        await connectToDB();
+        const currency = await CurrencyModel.updateOne({
+            symbol
+        },{
+            symbol,
+            name:{
+                en:name?.en,
+                fa:name?.fa
+            },
+            currencyType,
+            price,
+            cap,
+            percentChangeWeek,
+            percentChangeDay
+        });
+    } catch (err:any) {
+        throw new err.message
+    }
+};
+
+
+export const FindCurrencyByeSymbol = async (symbol:string) => {
+    try {
+        connectToDB();
+        const currency = await CurrencyModel.findOne({
+            symbol
+        });
+        return currency
+    } catch (err:any) {
+        throw err.message
+    }
+}
