3be3f6c6f49e7a8e150a258599a89339235038d3
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-25 12:47:55 +0330

    feat : add get all query funnction

--- db/currency/currency.utils.ts
+++ db/currency/currency.utils.ts
@@ -77,3 +77,13 @@ export const FindCurrencyByeSymbol = async (symbol:string) => {
         throw err.message
     }
 }
+
+export const FindAllCurrency = async (from:number,to:number) => {
+    try {
+        await connectToDB();
+        return CurrencyModel.find({}).skip(from-1).limit(to-from+1);
+    } catch (err:any) {
+        console.log("error in FindAllCurrency-db-utils : ",err.message);
+        throw new Error(err.message);
+    }
+};
