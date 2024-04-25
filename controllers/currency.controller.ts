dc43ab701923bda3fa85cb5b7ce225627fdedca1
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-25 12:46:53 +0330

    feat : add get all currencies handler

--- controllers/currency.controller.ts
+++ controllers/currency.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { FindCurrencyByeSymbol } from "../db/currency/currency.utils";
+import { FindAllCurrency, FindCurrencyByeSymbol } from "../db/currency/currency.utils";
+import { CurrencyType } from "../models/currency.model";
 
 
 export async function getByeSymbolHandler(req:Request, res:Response) {
@@ -12,6 +13,24 @@ export async function getByeSymbolHandler(req:Request, res:Response) {
         return res.status(200).json({
             message:"success",
             currency
+        });
+    } catch (err:any) { 
+        return res.status(500).json({message:"some thing went wrong"});
+    }
+}
+
+export async function getAllCurrencylHandler(req:Request, res:Response) {
+    const from = Number(req.query.from) || 1;
+    const to = Number(req.query.to) || 10;
+
+    if (to - from < 0) return res.status(400).json({message:"invalid params"});
+
+    try {
+        const currencies : CurrencyType[] = await FindAllCurrency(from,to);
+        console.log("getAllCurrencylHandler : ",currencies.length);
+        return res.status(200).json({
+            message:"success",
+            currencies
         });
     } catch (err:any) { 
         return res.status(500).json({message:"some thing went wrong"});
