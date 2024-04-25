0213ac62f0174cb44449a7482984e1bdba341f9d
sobhanhsa <sobhanb7b7@protonmail.com>
2024-04-25 12:15:55 +0330

    feat : add syncer and router

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ configDotenv()
 import express from "express";
 import cookieParser from "cookie-parser";
 import userRouter from "./routers/user.router";
+import { SyncGlobalCrypto } from "./providers/currency/cryptoCurrency.provider";
+import currencyRouter from "./routers/currency.router";
 
 const port = process.env.PORT || 8000;
 
@@ -14,5 +16,13 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/user",userRouter);
+app.use("/currency",currencyRouter);
 
-app.listen(port, () => console.log("server in running on port %d",port));
+app.listen(port, () => {
+    console.log("server in running on port %d",port)
+    try {
+        SyncGlobalCrypto();
+    } catch (err:any) {
+        console.log("error in on listen : ",err.message);
+    }
+});
